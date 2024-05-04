e54df7784b0e0880996e727fbf9de8b6c716bbe9
chanhl22 <lch9502@naver.com>
2024-05-04 11:49:25 +0900

    refactor: 불필요한 코드 삭제

--- src/test/java/moais/todolist/domain/todo/TodoRepositoryTest.java
+++ src/test/java/moais/todolist/domain/todo/TodoRepositoryTest.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.Sort;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TodoRepositoryTest {
