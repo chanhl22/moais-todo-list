67323947545f23d57a3bf9b28344507bbd28dd55
chanhl22 <lch9502@naver.com>
2024-05-05 12:28:19 +0900

    feat: 회원 TODO 리스트 조회 응답 변경 후 화면 수정

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -12,7 +12,7 @@ new Vue({
 
             axios.get('/todo/list?page=0&size=20')
                 .then(function (response) {
-                    $this.todolist = response.data.data;
+                    $this.todolist = response.data.data.todos;
                 })
                 .catch(function (error) {
                     alert("오류발생");
